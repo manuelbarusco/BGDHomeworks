409e4fdbccdeff278732d7247e246150ee332c47
riccardorampon <38914771+riccardorampon@users.noreply.github.com>
2022-04-08 21:57:01 +0200

    commenti finali

--- G045HW1.java
+++ G045HW1.java
@@ -7,36 +7,37 @@ import scala.Tuple2;
 import java.io.IOException;
 import java.util.*;
 
+/*
+GROUP: 045
+COMPONENTS:
+- Barusco Manuel
+- Gregori Andrea
+- Rampon Riccardo
+ */
+
 public class G045HW1 {
 
-    //Comparator class for sorting productPopularity1 and 2 JavaPairRDDs
+    //Comparator class for sorting productPopularity1 and productPopularity2 JavaPairRDDs
     public static class ProductPopularityComparator implements Comparator<Tuple2<String,Long>>{
-
         @Override
         public int compare(Tuple2<String, Long> o1, Tuple2<String, Long> o2) {
             return o1._1().compareTo(o2._1());
         }//compare
     }//ProductPopularityComparator
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IllegalArgumentException, IOException {
 
         //simple check of the command line arguments
         if (args.length != 4) {
             throw new IllegalArgumentException("USAGE: K H S dataset_path");
         }//if
 
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         // SPARK SETUP
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
         SparkConf conf = new SparkConf(true).setAppName("HW1");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        sc.setLogLevel("WARN");     //Reduce the warning
+        sc.setLogLevel("WARN");
 
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         // COMMAND LINE INPUT READING
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
         int K = Integer.parseInt(args[0]);      // Read number of partitions
         int H = Integer.parseInt(args[1]);      // Read number H
         String S = args[2];                     // Read country S
@@ -44,12 +45,10 @@ public class G045HW1 {
         // Read input file and subdivide it into K random partitions (cache() forces the system to compute the RDD)
         JavaRDD<String> rawData = sc.textFile(args[3]).repartition(K).cache();
 
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         // SETTING GLOBAL VARIABLES
-        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
         long numRows, numProductCustumer;
         Random randomGenerator = new Random();
+
         numRows = rawData.count(); // number of elements of the rawData RDD
         System.out.println("Number of rows = " + numRows);
 
@@ -64,15 +63,11 @@ public class G045HW1 {
                         (transaction) -> {
                             String[] tokens = transaction.split(",");
                             if (S.equalsIgnoreCase("all")){
-                                if (Integer.parseInt(tokens[3])>0) //Checking quantity > 0
-                                    return true;
-                                else
-                                    return false;
-                            }//if
-                            if (Integer.parseInt(tokens[3])>0 && tokens[7].equalsIgnoreCase(S)) //Checking quantity > 0 and Country = S
-                                return true;
-                            else
-                                return false;
+                                //Checking quantity > 0
+                                return Integer.parseInt(tokens[3]) > 0;
+                            }
+                            //Checking quantity > 0 and Country = S
+                            return Integer.parseInt(tokens[3]) > 0 && tokens[7].equalsIgnoreCase(S);
                         }
                 )
                 .mapToPair(
@@ -89,45 +84,43 @@ public class G045HW1 {
                             return pair;
                         }
                 );
+
         numProductCustumer = productCustomer.count();
         System.out.println("Product-Customer Pairs = " + numProductCustumer);
 
         //productPopularity1
-        productPopularity1 = productCustomer
-                .mapToPair((pc) -> {    // <-- MAP-PHASE (R1): EMPTY
-                    return pc;
-                })
-                .mapPartitionsToPair((productCustomerPair) -> { // <-- REDUCE PHASE (R1)
+        productPopularity1 = productCustomer //MAP PHASE (R1) EMPTY
+                .mapPartitionsToPair((productCustomerPair) -> { //REDUCE PHASE (R1)
 
-                    //execute counts in each partition
+                    //execute counts of product-popularity in each partition
                     HashMap<String, Long> counts = new HashMap<>();
                     while(productCustomerPair.hasNext()) {
                         Tuple2<String, Integer> tuple= productCustomerPair.next();
                         counts.put(tuple._1(), 1L+ counts.getOrDefault(tuple._1(), 0L));
                     }//while
 
-                    //create the output
+                    //create the output for the next round
                     ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
                     for (Map.Entry<String, Long> e : counts.entrySet()) {
                         pairs.add(new Tuple2<>(e.getKey(), e.getValue()));
                     }//for
 
                     return pairs.iterator();
-                })
-                .mapPartitionsToPair((pc) -> {    // <-- MAP-PHASE (R2): EMPTY
-                    return pc;
-                })
-                .reduceByKey((x,y) -> x+y); // <-- REDUCE PHASE (R2)
+                }) //MAP PHASE (R2) EMPTY
+                .groupByKey().mapValues((it) -> { //REDUCE PHASE (R2)
+                    long sum=0L;
+                    for(long c : it){
+                        sum+=c;
+                    }//for
+                    return sum;
+                });
 
         //productPopularity2
-        productPopularity2 = productCustomer
-                .mapToPair( (prodCust) -> { // <-- MAP-PHASE (R1): EMPTY
-                    return prodCust;
-                })
-                .groupBy( (prodCustPair) -> randomGenerator.nextInt(K)) // <-- KEY ASSIGNMENT + SHUFFLE + GROUPING
-                .flatMapToPair((element) -> { // <-- REDUCE PHASE (R1)
+        productPopularity2 = productCustomer //MAP PHASE (R1) EMPTY
+                .groupBy( (prodCustPair) -> randomGenerator.nextInt(K)) //KEY ASSIGNMENT+SHUFFLE+GROUPING
+                .flatMapToPair((element) -> { //REDUCE PHASE (R1)
 
-                    //execute counts in each partition
+                    //counts for each partitions
                     HashMap<String, Long> counts = new HashMap<>();
                     for(Tuple2<String, Integer> c : element._2()){
                         counts.put(c._1(), 1L+ counts.getOrDefault(c._1(), 0L));
@@ -140,11 +133,8 @@ public class G045HW1 {
                     }//for
 
                     return pairs.iterator();
-                })
-                .mapToPair( pair -> { // <-- MAP-PHASE (R2): EMPTY
-                    return pair;
-                })
-                .reduceByKey((x,y) -> x+y); // <-- REDUCE PHASE (R2)
+                }) //MAP PHASE (R2) EMPTY
+                .reduceByKey((x,y) -> x+y); //REDUCE PHASE (R2)
 
         if(H==0) {
             //task6: print all the pairs in productPopularity1 in lexicographic order
@@ -165,14 +155,12 @@ public class G045HW1 {
         }//if
 
         if(H>0) {
-            rate = productPopularity1.mapToPair((pp1) -> {
-                return pp1.swap();
-            }).sortByKey(false);
-
+            //task5: save in a list and prints the ProductID and Popularity of the H products with highest Popularity
+            rate = productPopularity1.mapToPair((pp1) -> pp1.swap()).sortByKey(false);
             System.out.println("Top "+H+ " Products and their Popularities");
             ArrayList<Tuple2<Long, String>> rateList = new ArrayList<>(rate.take(H));
             for (Tuple2<Long, String> ppr : rateList) {
-                System.out.print("Product: " + ppr._2() + " Popularity: " + ppr._1() + "; ");
+                System.out.print("Product " + ppr._2() + " Popularity " + ppr._1() + "; ");
             }//for
         }//if
     }//main
